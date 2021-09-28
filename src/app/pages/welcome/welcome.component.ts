8b6ea06aa1168c2177ff25efaa41046f45b048c2
anathi best <anathi.best96@gmail.com>
2021-09-28 08:37:36 +0200

    front

--- src/app/pages/welcome/welcome.component.ts
+++ src/app/pages/welcome/welcome.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
@@ -8,6 +10,7 @@ import { Component, OnInit } from '@angular/core';
 export class WelcomeComponent implements OnInit {
 
   constructor() { }
+  tenants: any = {};
 
   ngOnInit(): void {
   }
